71f3c1a1660c15f0373f0172d9d149679f373261
SAYEM_19 <hide5too9@gmail.com>
2025-07-07 17:58:05 +0600

    Update and rename Intersecting rectangle to Intersecting rectangle.cpp

--- Basic Geometry/Intersecting rectangle.cpp
+++ Basic Geometry/Intersecting rectangle.cpp
@@ -1,6 +1,8 @@
 /* -- Intersecting rectangle when bottom-left and top-right corners of two rectangles are given -- */
-
-
+/*
+preoblem => https://codeforces.com/problemset/problem/1028/C 
+sol.     => https://codeforces.com/contest/1028/submission/327933200
+*/
 vector<ll> inter_rec(vector<ll> ua , vector<ll> ub){// ua = {lower_left_x ,lower_left_y ,upper_right_x ,upper_right_y};
 	vector<ll> ret = ua;
 	ret[0] = max(ua[0] , ub[0]) , ret[1] = max(ua[1] , ub[1]) , ret[2] = min(ua[2] , ub[2]) , ret[3] = min(ua[3] , ub[3]);
@@ -13,6 +15,3 @@ vector<ll> inter_rec(vector<ll> ua , vector<ll> ub){// ua = {lower_left_x ,lower
 		return ret;
 	}
 }
-
-preoblem => https://codeforces.com/problemset/problem/1028/C 
-sol.     => https://codeforces.com/contest/1028/submission/327933200
